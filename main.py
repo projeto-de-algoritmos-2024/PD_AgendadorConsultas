68aefb81b5639899baad4b0e63ccf8e51b830a94
Izabella Alves <iap21010705@gmail.com>
2025-02-08 14:39:39 -0300

    add funcao de converter pra minutos
    Co-authored-by: Gabriel Zaranza <GZaranza@users.noreply.github.com>

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("blue")
 
 PESOS = {"Baixa": 1, "Média": 2, "Alta": 3}
+
+def converter_para_minutos(horario_str):
+    try:
+        horario_obj = datetime.strptime(horario_str, "%H:%M")
+        return horario_obj.hour * 60 + horario_obj.minute
+    except ValueError:
+        messagebox.showerror("Erro", f"Horário inválido: {horario_str}. Use o formato HH:MM.")
+        return None
