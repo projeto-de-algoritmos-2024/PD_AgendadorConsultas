11258d4865293a069b7914aada5b7875c535a4b7
Izabella Alves <iap21010705@gmail.com>
2025-02-08 14:36:17 -0300

    cria arquivo python e define as prioridades
    Gabriel Zaranza <GZaranza@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,9 @@
+import customtkinter as ctk
+import tkinter as tk
+from tkinter import ttk, messagebox
+from datetime import datetime
+
+ctk.set_appearance_mode("Dark")
+ctk.set_default_color_theme("blue")
+
+PESOS = {"Baixa": 1, "Média": 2, "Alta": 3}
